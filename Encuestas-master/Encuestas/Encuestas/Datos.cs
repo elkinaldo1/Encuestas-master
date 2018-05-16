3dd3d9ca5708c7da0e875c06738ddc77237f0553
elkinaldo1 <38057843+elkinaldo1@users.noreply.github.com>
2018-05-15 21:38:56 -0500

    Initial commit

---
+++ Encuestas-master/Encuestas/Encuestas/Datos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Encuestas
+{
+    public class Datos : Notificable
+    {
+        #region Constructor
+
+        public Datos()
+        {
+
+        }
+
+        #endregion
+
+        #region Atributos
+
+        private ObservableCollection<Encuesta> encuestas;
+
+        #endregion
+
+        #region Propiedades
+
+        public ObservableCollection<Encuesta> Encuestas
+        {
+            get
+            {
+                return Encuestas;
+            }
+            set
+            {
+                if (encuestas == value)
+                {
+                    return;
+                }
+                encuestas = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Encuestas-master/Encuestas/Encuestas/Encuesta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encuestas
+{
+    public class Encuesta
+    {
+        #region Atributos
+
+        public string equipoFavorito;
+
+        #endregion
+
+        #region Propiedades
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string EquipoFavorito
+        {
+            get
+            {
+                return equipoFavorito;
+            }
+            set
+            {
+                equipoFavorito = value;
+                switch (equipoFavorito)
+                {
+                    case "Colombia":
+                        Color = "Yellow";
+                        break;
+
+                    case "Peru":
+                        Color = "Red";
+                        break;
+
+                    case "Brasil":
+                        Color = "Green";
+                        break;
+
+                    case "Rusia":
+                        Color = "Blue";
+                        break;
+
+                    case "Alemania":
+                        Color = "Black";
+                        break;
+
+                    case "Argentina":
+                        Color = "Orange";
+                        break;
+
+                }
+            }
+        }
+
+        public string Color { get; set; }
+
+        #endregion
+
+        #region Metodos
+
+        public override string ToString()
+        {
+            return $"{Nombre}|{FechaNacimiento}|{EquipoFavorito}";
+        }
+
+        #endregion
+
+    }
+}
